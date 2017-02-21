a10d08b2ad6ebc9c4cb755d189acdcebe18a5563
Julian LaNeve <lanevejulian@gmail.com>
2017-02-21 14:59:39 -0600

    Upgrade packages and change config

--- test/test-config.js
+++ test/test-config.js
@@ -1,6 +1,7 @@
 export default {
-	user: 'jlaneve',
-	token: process.env.TOKEN,
-	repo: 'all',
-    method: 'listRepoCommits'
+    token: process.env.TOKEN,
+    repo: 'aries-activity-github-source',
+    owner: 'aries-data',
+    number: 'all',
+    issue_number: 'all',
 };
