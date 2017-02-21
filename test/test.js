e7b96e46c6d13a4124aebb4a6801dc879b74177b
Julian LaNeve <lanevejulian@gmail.com>
2017-02-21 14:57:25 -0600

    Write tests for normal and `all` methods

--- test/test.js
+++ test/test.js
@@ -4,16 +4,50 @@ import config from './test-config';
 
 // example - make sure configuration is the same
 test('proper configuration', t => {
-    const activity = new GithubSource();
     t.equal(GithubSource.props.name, require('../package.json').name);
     t.equal(GithubSource.props.version, require('../package.json').version);
     t.end();
 });
 
-test('test method', async (t) => {
-    const { user, repo } = config;
+test('test individual methods', async (t) => {
     const source = new GithubSource();
     const client = source.authenticatedClient(config);
-    const data = await source.getAll(config, client);
-    t.ok(data);
+
+    const methods = [
+        'listIssues', 'listAssignees', 'listIssueComments',
+        'listRepoComments', 'listIssueEvents', 'listRepoEvents',
+        'listPullRequests', 'listMergedPullRequests', 'listRepos',
+        'listRepoCommitComments', 'listRepoCommits', 'listCollaborators',
+    ];
+
+    let promises = [];
+    methods.forEach(method => {
+        t.comment(method);
+        promises.push(source[method](config, client));
+    });
+    Promise.all(promises).then(results => {
+        results.forEach(res => { t.ok(res); t.comment(JSON.stringify(res)) });
+    }).catch(e => t.comment("ERROR: " + e));
+});
+
+
+test('test all repo field', async t => {
+    const source = new GithubSource();
+    const client = source.authenticatedClient(config);
+
+    const methods = [
+        'listIssues', 'listAssignees', 'listIssueComments',
+        'listRepoComments', 'listIssueEvents', 'listRepoEvents',
+        'listPullRequests', 'listMergedPullRequests', 'listRepos',
+        'listRepoCommitComments', 'listRepoCommits', 'listCollaborators',
+    ];
+
+    let promises = [];
+    methods.forEach(method => {
+        t.comment(method);
+        promises.push(source.getAll({ ...config, method }, client));
+    });
+    Promise.all(promises).then(results => {
+        results.forEach(res => { t.ok(res); t.comment(JSON.stringify(res)); });
+    }).catch(e => t.comment("ERROR: " + e));    
 });
