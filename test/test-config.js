8911f730365ebe17202de5f6cf44a36db26ec2fc
Mac McCann <johnwilfredmccann@gmail.com>
2017-06-23 18:17:44 -0400

    circle

--- test/test-config.js
+++ test/test-config.js
@@ -3,7 +3,6 @@ export default {
     method : "listOrgRepos",
     method : "listAssignees",
     repo : "allOrg",
-    token: '72ebdaa0f4281c373fc758cfc6a9c6cbc7e3485d',
     org : "aries-data",
     owner : "aries-data",
     issueNum : "1",
