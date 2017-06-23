5c4216c8bc7e780793464a3a50cf739b9329ad43
Mac McCann <johnwilfredmccann@gmail.com>
2017-06-23 16:55:42 -0400

    removed require dotenv

--- test/test-config.js
+++ test/test-config.js
@@ -1,11 +1,8 @@
-require('dotenv').config();
-
 export default {
-	token: process.env.TOKEN,
 	user : "macmccann",
     method : "listOrgRepos",
     method : "listAssignees",
-                repo : "allOrg",
+    repo : "allOrg",
     org : "aries-data",
     owner : "aries-data",
     issueNum : "1"
